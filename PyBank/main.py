aa71fed29a08f1de517355f8db495418a6b29ec4
yujit0518 <50118732+yujit0518@users.noreply.github.com>
2019-06-06 19:42:24 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,19 @@
 import csv
 
-# Files load and output 
+# Load the csv file and write code for output results
 file_load = "budget_data.csv"
-file_output = "analysis.txt"
+file_output = "Bank_Analysis.txt"
 
-# tracking the parameters
+# tracking the parameters and creating variables from existing data
 total_months = 0
 total_amount = 0
 previous = 0
 month_change = []
-profit_change_list = []
-max_increase = 0 
-max_decrease = 0
+profit_change = []
+greatest_increase = 0 
+greatest_decrease = 0
 
-# read the csv & convert to dictionaries 
+# read the cvs files
 with open(file_load) as profit_data:
     reader = csv.reader(profit_data)
 
@@ -21,27 +21,25 @@ with open(file_load) as profit_data:
 
     for row in reader:
 
-        # track total
+        # track total amount/months
         total_months = total_months + 1 
         total_amount = total_amount + int(row[1])
-
 
         if total_months > 1:
             change = int(row[1])-previous
             month_change.append(change)
 
-            if change > max_increase:
-                max_increase = change 
+            if change > greatest_increase:
+                greatest_increase = change 
                 max_increase_month = row[0]
 
-            if change < max_decrease:
-                max_decrease = change
+            if change < greatest_decrease:
+                greatest_decrease = change
                 max_decrease_month = row[0]
 
         previous = int(row[1])
 
-# Calculate average profit change
-
+# Calculate the resullt and changes
 average = round(sum(month_change)/(len(month_change)+ 0.0),2)
 
 output = ('Financial Analysis'+ '\n'+
@@ -49,10 +47,8 @@ output = ('Financial Analysis'+ '\n'+
           'Total Months: ' + str(total_months) + '\n' +
           'Total: ' + ' $'+ str(total_amount) + '\n' +
           'Average Change:' + ' $' + str(average) + '\n' +
-          'The Greatest Increase in Profits: ' + max_increase_month + ' $' + str(max_increase) + '\n' +
-          'The Greatest Decrease in Profits: ' + max_decrease_month + ' $' + str(max_decrease) + '\n')
-
-      
+          'Greatest Increase in Profits: ' + max_increase_month + ' $' + str(greatest_increase) + '\n' +
+          'Greatest Decrease in Losses: ' + max_decrease_month + ' $' + str(greatest_decrease) + '\n')
 
 print(output)
 
